9d00b8474c51a87b0b6bc66e84a591c55d0402cf
MRKS77 <artxmarkos@gmail.com>
2021-08-02 23:04:05 +0300

    feat: add structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,45 @@
 import React from "react";
+import { Button, Card, Col, Layout, Row } from "antd";
+import TodoForm from "./components/todoForm/TodoForm";
+const { Header } = Layout;
 
 function App() {
   return (
-    <div>
-      <h1>
-        TODO
-      </h1>
-    </div>
+    <>
+      <Header className="header">
+        <h1>TODO</h1>
+      </Header>
+      <Row
+        justify="center"
+        align="middle"
+        gutter={[0, 20]}
+        className="todos-container"
+      >
+        <Col
+          xs={{ span: 23 }}
+          sm={{ span: 21 }}
+          md={{ span: 19 }}
+          lg={{ span: 18 }}
+          xl={{ span: 17 }}
+        >
+          <Card title="Create a new note">
+            <TodoForm />
+          </Card>
+        </Col>
+
+        <Col
+          xs={{ span: 23 }}
+          sm={{ span: 21 }}
+          md={{ span: 19 }}
+          lg={{ span: 18 }}
+          xl={{ span: 17 }}
+        >
+          <Card title="Note List">
+            AAAAAAAAAAAAAA
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 }
 
---
+++ client/src/components/todoForm/TodoForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoForm = () => {
+    return (
+        <h1>
+            FOOOORM
+        </h1>
+    )
+}
+
+export default TodoForm;
